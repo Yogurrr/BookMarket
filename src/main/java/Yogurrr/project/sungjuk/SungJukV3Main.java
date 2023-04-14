969e5ab722cdb08cb31f722d74611e99dd8744d7
LeeSeungYeon <masakisqq@naver.com>
2023-04-14 10:18:18 +0900

    성적처리 프로그램 v3 - 성적 입력

--- src/main/java/Yogurrr/project/sungjuk/SungJukV3Main.java
+++ src/main/java/Yogurrr/project/sungjuk/SungJukV3Main.java
@@ -1,13 +1,14 @@
 package Yogurrr.project.sungjuk;
 
 import Yogurrr.project.sungjuk.service.SungJukV1cService;
-import Yogurrr.project.sungjuk.service.SungJukV2ServiceImpl;
+import Yogurrr.project.sungjuk.service.SungJukV3ServiceImpl;
 
-public class SungJukV2Main {
-    // 성적 처리프로그램 v2
-    // 배열과 클래스, 인터페이스, 예외처리, 컬렉션을 이용해서
+public class SungJukV3Main {
+    // 성적 처리프로그램 v3
+    // 배열과 클래스, 인터페이스, 예외처리, 컬렉션, 파일처리를 이용해서
     // 성적을 입력하면 총점, 평균, 학점을 계산함
-    // SungJukV2Main, SungJukVO, SungJukV1cService, SungJukV2ServiceImpl
+    // SungJukV2Main, SungJukVO, SungJukV1cService, SungJukV3ServiceImpl
+    // sungjukv3.dat
     // 1. 성적 데이터 입력
     // 2. 성적 데이터 조회         (이름, 국어, 영어, 수학)
     // 3. 성적 데이터 상세 조회    (모두 다 출력)
@@ -16,7 +17,7 @@ public class SungJukV2Main {
     // 0. 프로그램 종료
 
     public static void main(String[] args) {
-        SungJukV1cService sjsrv = new SungJukV2ServiceImpl();
+        SungJukV1cService sjsrv = new SungJukV3ServiceImpl();
 
         while (true) {
             int menu = sjsrv.displayMenu();
