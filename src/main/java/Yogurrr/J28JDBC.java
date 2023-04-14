70584ff8ee10b949e8178f39086584b1dc485821
LeeSeungYeon <masakisqq@naver.com>
2023-04-14 17:51:27 +0900

    도서정보 adv select (수정)

--- src/main/java/Yogurrr/J28JDBC.java
+++ src/main/java/Yogurrr/J28JDBC.java
@@ -39,6 +39,7 @@ public class J28JDBC {
                 Book book = new Book(rs.getInt(1),
                 rs.getString(2), rs.getString(3),
                         rs.getInt(4), rs.getString(5));
+                bookdata.add(book);
             }
 
         } catch (SQLException e) {
