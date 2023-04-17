105ddc1c8af2b16142644a0d73bfcfd76417d72c
LeeSeungYeon <masakisqq@naver.com>
2023-04-17 10:18:39 +0900

    도서정보 delete

--- src/main/java/Yogurrr/J30JDBC.java
+++ src/main/java/Yogurrr/J30JDBC.java
@@ -5,22 +5,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class J29JDBC {
+public class J30JDBC {
     private static String DRV = "org.mariadb.jdbc.Driver";
     private static String URL = "jdbc:mariadb://fullstacks.cvdlpxnz2ebi.ap-northeast-2.rds.amazonaws.com:3306/fullstacks";
     private static String USR = "admin";
     private static String PWD = "fullstack_2023";
 
-    private static String selectBookSQL= "select * from newbooks where title like ? order by bookno desc";
+    private static String deleteBookSQL= "delete * from newbooks where bookno = ?";
 
     public static void main(String[] args) {
-        // newbooks 테이블에서 도서명에 'IT CookBook'이 포함된 레코드 조회
+        // newbooks 테이블에서 입력 받은 도서번호를 이용해서 레코드 삭제
         List<Book> bookdata = new ArrayList<>();
 
-        // 검색할 도서명을 입력 받음
+        // 삭제할 도서번호를 입력 받음
         Scanner sc = new Scanner(System.in);
-        System.out.println("조회할 도서명은? ");
-        String findbook = sc.nextLine();   // 입력 받아야 할 값이 한 어절 이상이면 sc.nextLine()으로 받아야 함
+        System.out.println("삭제할 도서번호는? ");
+        int bookno = sc.nextInt();
 
         try {
             Class.forName(DRV);
@@ -30,34 +30,21 @@ public class J29JDBC {
 
         Connection conn = null;
         PreparedStatement pstmt = null;
-        ResultSet rs = null;
 
         try {
             conn = DriverManager.getConnection(URL, USR, PWD);
-            pstmt = conn.prepareStatement(selectBookSQL);
-            pstmt.setString(1, "%" + findbook + "%");
+            pstmt = conn.prepareStatement(deleteBookSQL);
+            pstmt.setInt(1, bookno);
 
-            // SQL문 실행 후 결과집합 받음
-            rs = pstmt.executeQuery();  // DML 실행시 사용
-
-            while(rs.next()) {
-                Book book = new Book(rs.getInt(1),
-                rs.getString(2), rs.getString(3),
-                        rs.getInt(4), rs.getString(5));
-                bookdata.add(book);
-            }
+            // SQL문 실행 후 결과 확인
+            int cnt = pstmt.executeUpdate();
+            System.out.println("데이터 삭제 확인 : " + cnt);
 
         } catch (SQLException e) {
             System.out.println("디비 접속주소나 아이디/비번, SQL문을 확인하세요!!");
         } finally {
-            if (rs != null) try { rs.close(); } catch (Exception ex) {}
             if (pstmt != null) try { pstmt.close(); } catch (Exception ex) {}
             if (conn != null) try { conn.close(); } catch (Exception ex) {}
-        }
-
-        // 도서정보 출력
-        for (Book b : bookdata) {
-            System.out.println(b);
         }
     }
 }
